a7fa7ba02a2a7f0e36434c3aa8994c582f01d465
David Amaro <dav1494@ciencias.unam.mx>
2013-06-23 16:20:29 -0500

    Ya conoce tablas de varias columnas, falta asignar inteligentemente los valores obtenidos

--- hack.pl
+++ hack.pl
@@ -14,24 +14,52 @@ open my $octave, '>>', $octave_file
 
 my @x_axis = ();
 my @y_axis = ();
+my @z_axis = ();
+my $linea;
 
-while ( my $line = <$tables> ) {
-    if ( $line =~ /\\hline\s(-?\d+\.-?\d+)\s\&\s(-?\d+\.-?\d+)/ ) {
-        push @x_axis, $1;
-        push @y_axis, $2;
+while ( $linea = <$tables> ) {
+    if ( $linea =~ /(\\hline.*)/ ) {
+        $linea = $1;
+        last;
     }
 }
 
+print "$linea\n";
+
+my $contador = 0;
+$contador++ while $linea =~ /[[:digit:].]+/g;
+
+print "Contador: '$contador'\n";
+
+my $reg_exp = "\\\\hline\\s";
+
+for ( 1 .. $contador ) {
+    $reg_exp .= "([[:digit:].-]+)";
+    if ( $_ == $contador ) {
+        last;
+    }
+    $reg_exp .= "\\s\\&\\s";
+}
+print "$reg_exp\n";
+
+while ( my $line = <$tables> ) {
+    if ( $line =~ /$reg_exp/ ) {
+        push @x_axis, $1;
+        push @y_axis, $2;
+        push @z_axis, $3;
+    }
+}
 #Write the useful information for Octave
 print $octave "x = [@x_axis];\n";
 print $octave "y = [@y_axis];\n";
-print $octave "plot(x,y,'.','markersize',15)\n;";
-print $octave "grid on\n";
-print $octave "xlabel('t(s)')\n";
-print $octave "ylabel('y\(m\)')\n";
+print $octave "z = [@z_axis];\n";
+#print $octave "plot(x,y,'.','markersize',15)\n;";
+#print $octave "grid on\n;";
+#print $octave "xlabel('t(s)')\n";
+#print $octave "ylabel('y\(m\)')\n";
 
 #Run Octave's script
-system ("octave", "--persist", "$octave_file");
+system ("octave", "--silent", "--persist", "$octave_file");
 
 close $octave or die "Could not close '$octave_file': $!";
 close $tables or die "Could not close '$filename': $!";
