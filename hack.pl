9dc2f85f680eb355c4dd7e1bc72f7a9ae3586d14
David Amaro <dav1494@ciencias.unam.mx>
2013-06-27 12:56:30 -0500

    Agregado soporte para documentos con múltiples tablasAgregada la expresión regular... ya no más undef :)

--- hack.pl
+++ hack.pl
@@ -6,11 +6,39 @@ use diagnostics;
 use Data::Dumper;
 use Regexp::Common;
 
+# Tamaño de los carácteres de la
+# declaración en LaTeX de las tablas
 use constant LONGTABLE => 14;
 use constant TABLE => 12;
 
 #Names
-my @names = qw(array_a array_b array_c array_d);
+my
+@names
+=
+qw(array_a
+array_b
+array_c
+array_d
+array_e
+array_f
+array_g
+array_h
+array_i
+array_j
+array_k
+array_l
+array_m
+array_n
+array_o
+array_p
+array_q
+array_r
+array_s
+);
+my $inicio = 0;
+my $adicion = 0;
+my $asesino = 0;
+my @valores = ();
 
 #Open file with LaTeX data
 my $latexfile = shift @ARGV
@@ -25,64 +53,108 @@ open my $octave, '>>', $octave_file
 
 # Referencias usadas para almacenar la información
 my @lista = (
-    [],
 );
+
 # Utiliza la declaración de la tabla
 # para conocer el número de columnas
 # y ver si se tiene que eliminar el primero 
 # y último para el longtable
-my $decision;
-my $linea;
-my $contador = 0;
 
-while ( $linea = <$tables> ) {
+sub numero_columnas {
+    my $linea = shift;
+    my $columnas = 0;
     if ( $linea =~ /(\\begin\{tabular\}.*)/ ) {
-        $linea = $1;
-        $contador++ while $linea =~ /[[:alpha:]]/g;
-        $contador -= TABLE;
-        $decision = 0;
-        last;
+        $columnas++ while $linea =~ /[[:alpha:]]/g;
+        $columnas -= TABLE;
     }
     elsif ( $linea =~ /(\\begin\{longtable\}.*)/) {
-        $linea = $1;
-        $contador++ while $linea =~ /[[:alpha:]]/g;
-        $contador -= LONGTABLE;
-        $decision = 1;
-        last;
+        $columnas++ while $linea =~ /[[:alpha:]]/g;
+        $columnas -= LONGTABLE;
     }
+    return $columnas;
 }
 
 # Agregar el número de arrays para las columnas
-for ( 1 .. ($contador - 1) ) {
-    push @lista, [];
+sub add_space {
+    my $columnas = shift;
+    $columnas -= 1;
+    foreach ( 0 .. $columnas ) {
+        push @lista, [];
+    }
 }
 
 # Ignora las lineas que no tengan hline
 # y asigna asigna los valores a cada
 # referencia.
-# Bug: Se van algunos undef.
+
+sub alfa_omega {
+    my @array = @lista;
+    my $contador = 0;
+    $contador++ while ( exists $lista[$contador]->[0] );
+    return $contador;
+}
+
+sub get_type {
+    my $linea = shift;
+    my $columnas = numero_columnas($linea);
+    #print "Adición: $adicion\n";
+    chomp $linea;
+    if ( $linea =~ /end(\{tabular\}|\{longtable\})/ ) {
+        #print "Salí\n";
+        $inicio += $adicion;
+        push @valores, $adicion;
+        $asesino++;
+        return;
+    }
+    elsif ( $linea =~ /(?=tabular|longtable)/ ) {
+        add_space($columnas);
+        $adicion = $columnas;
+        return;
+    }
+    elsif ( $linea =~ /\\hline.+?\z/ ) {
+        return $linea;
+    }
+    else {
+        return;
+    }
+}
+
 while ( my $line = <$tables> ) {
-    if ( $line =~ /(?=hline)/ ) { 
-        #my @m = ( $line =~ /([[:digit:].-]+)/g);
-        my @m = ( $line =~ /$RE{num}{real}/g);
-        for my $i ( 0 .. ($contador - 1) ) {
-            push $lista[$i], $m[$i];
-        }
+    my $valor = get_type($line);
+    next if !$valor;
+    my @datos = ( $line =~ /$RE{num}{real}/g);
+    my $final = $inicio + $adicion - 1;
+    #print "$inicio - $adicion - $final\n";
+    foreach my $index ( $inicio .. $final ) {
+        push $lista[$index], $datos[$index - $inicio];
     }
 }
-if ($decision) {
-    my $conter = $lista[0];
-    foreach my $i ( 0 .. ( $contador - 1) ) {
-        pop $lista[$i];
-        shift $lista[$i];
-    }
-}
+
+#print $asesino;
+#print "@valores\n";
+#print Dumper(@lista);
+
 #Write the useful information for Octave
-my $conter = $lista[0];
-foreach my $i ( 0 .. ($contador - 1) ) {
+#my $conter = $lista[0];
+# Número de columnas
+#foreach my $i ( 0 .. ($contador - 1) ) {
+#    print $octave "$names[$i] = [";
+#    # Número de elementos
+#    foreach my $j ( 0 .. ( $#$conter ) ) {
+#        print $octave " $lista[$i][$j] ";
+#    }
+#    print $octave "];\n";
+#}
+my $tamano = 0;
+$tamano++ while ( exists $lista[$tamano]->[0] );
+$tamano--;
+
+for my $i ( 0 .. $tamano ) {
+    my $j = 0;
     print $octave "$names[$i] = [";
-    foreach my $j ( 0 .. ( $#$conter ) ) {
-        print $octave " $lista[$i][$j] ";
+    while ( exists $lista[$i]->[$j] ) {
+        print $octave " $lista[$i]->[$j] ";
+        $j++;
     }
     print $octave "];\n";
 }
